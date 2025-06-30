cb6a753ce0cdd9dcea1d604011bcdc28b3d3dbc2
Umar Mubarak <mubyumar@gmail.com>
2025-06-30 11:11:16 +0100

    fifth commit

--- script.js
+++ script.js
@@ -23,83 +23,122 @@ GOOD LUCK 😀
 // console.log(BMI);
 
 
-const johnMass = 78;
-const johnHeight = 1.69;
+// const johnMass = 78;
+// const johnHeight = 1.69;
 
-const markMass = 92;
-const markHeight = 1.69;
-
-
-const johnBMI = johnMass / johnHeight ** 2;
-const markBMI = markMass / markHeight ** 2;
-
-const markHigherBMI = markBMI > johnBMI;
+// const markMass = 92;
+// const markHeight = 1.69;
 
 
-console.log(johnBMI);
-console.log(markBMI);
-console.log(markHigherBMI);
+// const johnBMI = johnMass / johnHeight ** 2;
+// const markBMI = markMass / markHeight ** 2;
 
-const johnMassTwo = 85;
-const johnHeightTwo = 1.76;
-
-const markMassTwo = 95;
-const markHeightTwo = 1.88;
-
-const johnBMITwo = johnMassTwo / johnHeightTwo ** 2;
-const markBMITwo = markMassTwo / markHeightTwo ** 2;
-
-const markTwoHigherBMI = markBMITwo > johnBMITwo;
-
-console.log(johnBMITwo);
-console.log(markBMITwo);
-console.log(markTwoHigherBMI);
+// const markHigherBMI = markBMI > johnBMI;
 
 
-// Type conversion
-const inputYear = '1991';
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
+// console.log(johnBMI);
+// console.log(markBMI);
+// console.log(markHigherBMI);
+
+// const johnMassTwo = 85;
+// const johnHeightTwo = 1.76;
+
+// const markMassTwo = 95;
+// const markHeightTwo = 1.88;
+
+// const johnBMITwo = johnMassTwo / johnHeightTwo ** 2;
+// const markBMITwo = markMassTwo / markHeightTwo ** 2;
+
+// const markTwoHigherBMI = markBMITwo > johnBMITwo;
 
 
-// 5 falsy values : 0, '', undefined, null, NaN
+// console.log(johnBMITwo);
+// console.log(markBMITwo);
+// console.log(markTwoHigherBMI);
 
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean('mubee'));
-console.log(Boolean({}));
-console.log(Boolean(''));
 
-const money = 100;
-if (money) {
-    console.log("don't spend it all.");
-} else {
-    console.log("You should get a job");
-}
+// // Type conversion
+// const inputYear = '1991';
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
 
-let height = 123;
-if (height) {
-    console.log('YAY! height is defined');
-} else {
-    console.log('Height is undefined');
-}
+
+// // 5 falsy values : 0, '', undefined, null, NaN
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('mubee'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+// const money = 100;
+// if (money) {
+//     console.log("don't spend it all.");
+// } else {
+//     console.log("You should get a job");
+// }
+
+// let height = 123;
+// if (height) {
+//     console.log('YAY! height is defined');
+// } else {
+//     console.log('Height is undefined');
+// }
 
 // Equality Operators
 
-const favorite = Number(prompt("what's your favorite number?"));
-console.log(favorite);
-console.log(typeof favorite);
+// const favorite = Number(prompt("what's your favorite number?"));
+// console.log(favorite);
+// console.log(typeof favorite);
 
-if (favorite === 23) {
-    console.log('Cool! 23 is the right answer');
-} else if (favorite === 7) {
-    console.log('7 is also a cool number');
-} else if (favorite === 9) {
-    console.log('9 is also a cool number');
+// if (favorite === 23) {
+//     console.log('Cool! 23 is the right answer');
+// } else if (favorite === 7) {
+//     console.log('7 is also a cool number');
+// } else if (favorite === 9) {
+//     console.log('9 is also a cool number');
+// } else {
+//     console.log('Number is not 23, or 7 or 9');
+// }
+
+// if (favorite !== 23) {
+//     console.log('Why not 23?');
+// }
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / heightJohn * heightJohn;
+console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is greater than John's (${BMIJohn})`);
 } else {
-    console.log('Number is not 23, or 7 or 9');
+    console.log(`John's BMI (${BMIJohn}) is greater than Mark's (${BMIMark})`);
 }
 
-if (favorite !== 23) {
-    console.log('Why not 23?');
+// LOGICAL OPERATORS
+
+const hasDriversLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDriversLicense);
+console.log(hasGoodVision);
+
+// if (hasDriversLicense && hasGoodVision) {
+//     console.log('yay!! you can drive');
+// } else {
+//     console.log('opps!! someone should drive...');
+// }
+
+const isTired = false;
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log('yay!! you can drive');
+} else {
+    console.log('oops!! someone should drive...');
 }
